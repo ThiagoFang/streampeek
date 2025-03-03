5580212443b395fbbf7744909499b5ca4a8ef925
ThiagoFang <thiagofang@gmail.com>
2025-03-03 18:11:25 -0300

    tailwind variables settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 function App() {
   return (
-    <div className="bg-red-500 text-white text-2xl">
-      <h1>Stream Peek</h1>
+    <div className="w-full gap-8 h-dvh p-4 flex flex-col items-center justify-center">
+      <img src="/logo_streampeek.png" alt="Logo StreamPeek" className="w-6" />
+
+      <div>
+        <h1 className="text-lg/5 font-medium text-center text-foreground">
+          Bem vindo ao <b className="font-bold">StreamPeek</b>
+        </h1>
+        <p className="text-center text-muted-foreground">
+          Acompanhe os seus streamers favoritos
+        </p>
+      </div>
+      <button className="p-2 font-medium rounded-md bg-primary w-full text-background">
+        Entrar com <span className="font-bold">Twitch.tv</span>
+      </button>
     </div>
   );
 }
