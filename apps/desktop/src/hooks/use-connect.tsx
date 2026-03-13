67dadfbde993a8229f63bd46e578c09f9b1ad49d
Thiago Fang <thiagofang@gmail.com>
2026-03-13 16:20:44 -0300

    refac: rework on connect logic (moved to a hook)

---
+++ apps/desktop/src/hooks/use-connect.tsx
@@ -0,0 +1,43 @@
+import { userApi } from "@/api/user";
+import { usePathStore } from "@/store/path";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+
+const POLLING_INTERVAL = 2000;
+
+function useConnect() {
+  const [isPolling, setIsPolling] = useState(false);
+  const navigate = usePathStore((state) => state.setPath);
+
+  const { mutate: connect } = useMutation({
+    mutationFn: userApi.getAuthUrl,
+    onSuccess: ({ url }) => {
+      open(url);
+      setIsPolling(true);
+    },
+    onError: (error) => {
+      console.error("[useConnect]", error);
+    },
+  });
+
+  const { data } = useQuery({
+    queryKey: ["auth", "status"],
+    enabled: isPolling,
+    queryFn: userApi.getAuthStatus,
+    refetchInterval: (query) => {
+      // if the user is authenticated, stop polling
+      return query.state.data?.authenticated ? false : POLLING_INTERVAL;
+    },
+  });
+
+  useEffect(() => {
+    if (data?.authenticated) {
+      setIsPolling(false);
+      navigate("home");
+    }
+  }, [data?.authenticated]);
+
+  return { connect, isPolling };
+}
+
+export { useConnect };
---
+++ apps/desktop/src/routes/auth.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { useConnect } from "@/hooks/use-connect";
+
+export function Auth() {
+  return (
+    <section className="w-full gap-12 h-dvh p-4 flex flex-col items-center justify-center">
+      <AuthContent />
+      <AuthButton />
+    </section>
+  );
+}
+
+function AuthContent() {
+  return (
+    <>
+      <img src="/logo_streampeek.png" alt="Logo StreamPeek" className="w-10" />
+
+      <div className="space-y-2">
+        <h1 className="text-lg/5 font-medium text-center text-foreground">
+          Bem vindo ao <b className="font-extrabold">StreamPeek</b>
+        </h1>
+        <p className="text-center leading-5 tracking-wide max-w-xs text-sm mx-auto text-muted-foreground">
+          Esteja sempre conectado com os streamers que você mais gosta
+        </p>
+      </div>
+    </>
+  );
+}
+
+function AuthButton() {
+  const { connect, isPolling } = useConnect();
+
+  return (
+    <Button className="w-full cursor-pointer" disabled={isPolling} onClick={() => connect()}>
+      {isPolling ? (
+        "Conectando..."
+      ) : (
+        <span className="font-medium">
+          Entrar com <span className="font-extrabold">Twitch.tv</span>
+        </span>
+      )}
+    </Button>
+  );
+}
